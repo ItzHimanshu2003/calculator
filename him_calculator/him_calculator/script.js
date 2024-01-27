568bfb9ff10248b04318c67ea1a22047855f4717
ItzHimanshu2003 <157607607+ItzHimanshu2003@users.noreply.github.com>
2024-01-27 21:27:02 +0530

    Add files via upload

---
+++ him_calculator/him_calculator/script.js
@@ -0,0 +1,29 @@
+const btns =document.getElementsByTagName('button');
+const screen =document.getElementById('screen');
+console.log(btns);
+
+for(let i=0;i<btns.length;i++){
+    btns[i].addEventListener('click',(e) =>{
+        console.log(e.target.innerText);
+        var text = e.target.innerText;
+
+        if(text === 'x'){
+            screen.value += '*';
+        }
+        else if(text === 'C'){
+            screen.value= '';
+        }
+         else if(text=== '='){
+            try{
+            var ans = eval(screen.value);
+            screen.value = ans;
+        } catch (error){
+            alert("Invalid Operation");
+            //screen.value="Invalid";
+        }
+    }else{
+            screen.value += text; 
+        }
+        
+    })
+}
